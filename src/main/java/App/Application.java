54d9a896fd27d70d19b67793b7747bd4c6b83437
fexicus <graxow.d@yandex.ru>
2023-06-06 00:08:53 +0300

    Немного рефакторинга.

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -11,55 +11,92 @@ import java.util.List;
 
 public class Application {
     public static final String URL = "https://hh.ru/vacancies/programmist_java";
-
-    public static void main(String[] args) throws IOException {
-        List<Vacancy> vacancies = parseVacancies();
+    public static void main(String[] args) throws IOException, NullPointerException {
+        List<Vacancy> vacancies = parseVacancies(2);
         for (int i = 0; i < vacancies.size(); i++) {
             Vacancy vacancy = vacancies.get(i);
             System.out.printf("Вакансия #%d - %s\n", i + 1, vacancy.getLinkOfVacancy());
             System.out.println(vacancy);
+            }
         }
-    }
 
-    private static List<Vacancy> parseVacancies() throws IOException {
+    private static List<Vacancy> parseVacancies(int numberOfPages) throws IOException, NullPointerException {
         List<Vacancy> vacancies = new ArrayList<>();
-        int numberOfPages = 2;
         for (int i = 0; i < numberOfPages; i++) {
             Document document = Jsoup.connect(i == 0 ? URL : URL + "?page=" + i).get();
-            Elements postTitleElements = document.getElementsByAttributeValue("data-qa", "serp-item__title");
-
-            for (Element postTitleElement : postTitleElements) {
-                String detailsLink = postTitleElement.attr("href");
-                Vacancy vacancy = new Vacancy();
-                vacancy.setLinkOfVacancy(detailsLink);
-                vacancy.setTitle(postTitleElement.text());
-                Document vacancyDetailsDocument = Jsoup.connect(detailsLink).get();
-                try {
-                    vacancy.setExperience(vacancyDetailsDocument.getElementsByClass("vacancy-description-list-item").first().text());
-                } catch (NullPointerException e) {
-                    vacancy.setExperience("Не указан");
-                }
-
-                Elements skillElements = vacancyDetailsDocument.getElementsByClass("bloko-tag__section bloko-tag__section_text");
-                List<String> skills = new ArrayList<>();
-                for (Element skillElement : skillElements) {
-                    String skill = skillElement.text();
-                    skills.add(skill);
-                }
-                vacancy.setSkills(skills);
-
-                vacancy.setAuthor(vacancyDetailsDocument.getElementsByClass("vacancy-company-name").first().text());
-
-                vacancy.setDateOfCreated(vacancyDetailsDocument.getElementsByClass("vacancy-creation-time-redesigned").first().text());
-                try {
-                    vacancy.setSalary(vacancyDetailsDocument.getElementsByAttributeValue("data-qa", "vacancy-salary").first().text());
-                } catch (NullPointerException e) {
-                    vacancy.setSalary("Не указана");
-                }
-                vacancies.add(vacancy);
-            }
+            parseVacancy(vacancies, document);
         }
         return vacancies;
     }
+
+    private static void parseVacancy(List<Vacancy> vacancies, Document document) throws IOException, NullPointerException {
+        Elements postTitleElements = document.getElementsByAttributeValue("data-qa", "serp-item__title");
+        for (Element postTitleElement : postTitleElements) {
+            String detailsLink = postTitleElement.attr("href");
+            Vacancy vacancy = new Vacancy();
+            vacancy.setLinkOfVacancy(detailsLink);
+            vacancy.setTitle(postTitleElement.text());
+            Document vacancyDetailsDocument = Jsoup.connect(detailsLink).get();
+
+            parseExperience(vacancy, vacancyDetailsDocument);
+            parseSkills(vacancy, vacancyDetailsDocument);
+            parseAuthor(vacancy, vacancyDetailsDocument);
+            parseDateOfCreated(vacancy, vacancyDetailsDocument);
+            parseSalary(vacancy, vacancyDetailsDocument);
+
+            vacancies.add(vacancy);
+        }
+    }
+
+    private static void parseSalary(Vacancy vacancy, Document vacancyDetailsDocument) {
+        try {
+            vacancy.setSalary(getElementByAttribute(vacancyDetailsDocument));
+        } catch (NullPointerException e) {
+            vacancy.setSalary("Не указана");
+        }
+    }
+
+    private static void parseExperience(Vacancy vacancy, Document vacancyDetailsDocument) {
+        try {
+            vacancy.setExperience(getElementByClass(vacancyDetailsDocument,"vacancy-description-list-item"));
+        } catch (NullPointerException e) {
+            vacancy.setExperience("Не указан");
+        }
+    }
+
+    private static void parseDateOfCreated(Vacancy vacancy, Document vacancyDetailsDocument) {
+        try {
+            vacancy.setDateOfCreated(getElementByClass(vacancyDetailsDocument,"vacancy-creation-time-redesigned"));
+        } catch (NullPointerException e){
+            vacancy.setDateOfCreated("Не указано");
+        }
+    }
+
+    private static void parseAuthor(Vacancy vacancy, Document vacancyDetailsDocument) {
+        try {
+            vacancy.setAuthor(getElementByClass(vacancyDetailsDocument, "vacancy-company-name"));
+        } catch (NullPointerException e) {
+            vacancy.setAuthor("Не указан");
+        }
+    }
+
+    private static void parseSkills(Vacancy vacancy, Document vacancyDetailsDocument) {
+        Elements skillElements = vacancyDetailsDocument.getElementsByClass("bloko-tag__section bloko-tag__section_text");
+        List<String> skills = new ArrayList<>();
+        for (Element skillElement : skillElements) {
+            String skill = skillElement.text();
+            skills.add(skill);
+        }
+        vacancy.setSkills(skills);
+    }
+
+    private static String getElementByAttribute(Document document) {
+        return document.getElementsByAttributeValue("data-qa", "vacancy-salary").first().text();
+    }
+
+    private static String getElementByClass(Document document, String className) {
+        return document.getElementsByClass(className).first().text();
+    }
+
 
 }
--- src/main/java/App/Vacancy.java
+++ src/main/java/App/Vacancy.java
@@ -9,44 +9,23 @@ public class Vacancy {
     private String dateOfCreated;
     private List<String> skills;
     private String salary;
-
     private String linkOfVacancy;
 
-    public String getTitle() {
-        return title;
-    }
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getExperience() {
-        return experience;
     }
 
     public void setExperience(String experience) {
         this.experience = experience;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
     public void setAuthor(String author) {
         this.author = author;
     }
 
-    public String getDateOfCreated() {
-        return dateOfCreated;
-    }
-
     public void setDateOfCreated(String dateOfCreated) {
         this.dateOfCreated = dateOfCreated;
-    }
-
-
-    public String getSalary() {
-        return salary;
     }
 
     public void setSalary(String salary) {
@@ -59,10 +38,6 @@ public class Vacancy {
 
     public void setLinkOfVacancy(String linkOfVacancy) {
         this.linkOfVacancy = linkOfVacancy;
-    }
-
-    public List<String> getSkills() {
-        return skills;
     }
 
     public void setSkills(List<String> skills) {
@@ -79,4 +54,7 @@ public class Vacancy {
                 "Ключевые навыки: " + skillsString + "\n" +
                 "Заработная плата: " + salary + "\n";
     }
+
+
+
 }
